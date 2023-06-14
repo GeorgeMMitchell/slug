f4edf3c1513414125236ab061677c6dd678af347
George Mitchell <georgemaxwellmitchell@outlook.com>
2023-06-14 14:59:20 -0500

    Added global macro testing

--- test/src/slug-test.cc
+++ test/src/slug-test.cc
@@ -1,11 +1,22 @@
 // Copyright (c) 2023, George Mitchell
 // See License.txt for license information
 
+#include <assert.h>
 #include <slug/log.h>
 
 #include <catch2/catch_all.hpp>
-
 #include <iostream>
+#include <string_view>
+
+namespace slug::test {
+
+struct config final {
+  static constexpr std::string_view out_filename = "slug-test-log.yml";
+  static constexpr std::wstring_view wout_filename = L"slug-test-log.yml";
+
+};  // config
+
+}  // namespace slug::test
 
 TEST_CASE("Logger Branching", "[branching]") {
   [[maybe_unused]] auto &&logger = slug::logger{std::clog};
@@ -37,4 +48,32 @@ TEST_CASE("Wide Logger Branching", "[branching]") {
 
   wlogger.open_console(std::wclog);
   wlogger.log(slug::trace, L"Writing wide message to console");
+}
+
+TEST_CASE("Global Macros", "[slug.globals]") {
+  static_assert(SLUG_GLOBAL && SLUG_W_GLOBAL,
+                "Both slug-test must be compiled with both native and wide "
+                "character global logging instances");
+
+  SLUG_SET_SEVERITY(slug::trace);
+
+  SLUG_LOG(slug::trace, "Test {} message from global logging instance",
+           "console");
+
+  SLUG_OPEN_FILE(slug::test::config::out_filename, std::ios::app);
+
+  SLUG_LOG(slug::trace, "Test {} message from global logging instance", "file");
+
+  SLUG_OPEN_CONSOLE(std::clog);
+
+//  SLUG_W_SET_SEVERITY(slug::trace);
+
+//  SLUG_W_LOG(slug::trace, L"Test {} message from global logging instance",
+//           "console");
+
+//  SLUG_W_OPEN_FILE(slug::test::config::out_filename, std::ios::app);
+
+//  SLUG_W_LOG(slug::trace, L"Test {} message from global logging instance", L"file");
+
+//  SLUG_W_OPEN_CONSOLE(std::wclog);
 }
